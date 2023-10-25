a48e715a7a087b792be4a9539ad925a694531b62
ronaldognunes <ronaldo2385nunes@gmail.com>
2023-10-25 20:48:25 -0300

    adicionando projeto

---
+++ fc-ddd-patterns-domain-events/src/infrastructure/customer/repository/sequelize/customer.repository.ts
@@ -0,0 +1,100 @@
+import Customer from "../../../../domain/customer/entity/customer";
+import Address from "../../../../domain/customer/value-object/address";
+import CustomerRepositoryInterface from "../../../../domain/customer/repository/customer-repository.interface";
+import CustomerModel from "./customer.model";
+import { TransactionInterface } from "../../../../domain/@shared/domain/transaction.interface";
+
+export default class CustomerRepository implements CustomerRepositoryInterface {
+
+  private transaction: TransactionInterface
+
+  public setTransaction(transaction: TransactionInterface): void {
+    this.transaction = transaction;
+  }
+
+  async create(entity: Customer): Promise<void> {
+    await CustomerModel.create({
+      id: entity.id,
+      name: entity.name,
+      street: entity.Address.street,
+      number: entity.Address.number,
+      zipcode: entity.Address.zip,
+      city: entity.Address.city,
+      active: entity.isActive(),
+      rewardPoints: entity.rewardPoints,
+    },
+      {
+        transaction: this.transaction.getTransaction()
+      }
+    );
+  }
+
+  async update(entity: Customer): Promise<void> {
+    await CustomerModel.update(
+      {
+        name: entity.name,
+        street: entity.Address.street,
+        number: entity.Address.number,
+        zipcode: entity.Address.zip,
+        city: entity.Address.city,
+        active: entity.isActive(),
+        rewardPoints: entity.rewardPoints,
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+        transaction: this.transaction.getTransaction()
+      }
+    );
+  }
+
+  async find(id: string): Promise<Customer> {
+    let customerModel;
+    try {
+      customerModel = await CustomerModel.findOne({
+        where: {
+          id,
+        },
+        rejectOnEmpty: true,
+        transaction: this.transaction.getTransaction()
+      });
+    } catch (error) {
+      throw new Error("Customer not found");
+    }
+
+    const customer = new Customer(id, customerModel.name);
+    const address = new Address(
+      customerModel.street,
+      customerModel.number,
+      customerModel.zipcode,
+      customerModel.city
+    );
+    customer.changeAddress(address);
+    return customer;
+  }
+
+  async findAll(): Promise<Customer[]> {
+    const customerModels = await CustomerModel.findAll({
+      transaction: this.transaction.getTransaction()
+    });
+
+    const customers = customerModels.map((customerModels) => {
+      let customer = new Customer(customerModels.id, customerModels.name);
+      customer.addRewardPoints(customerModels.rewardPoints);
+      const address = new Address(
+        customerModels.street,
+        customerModels.number,
+        customerModels.zipcode,
+        customerModels.city
+      );
+      customer.changeAddress(address);
+      if (customerModels.active) {
+        customer.activate();
+      }
+      return customer;
+    });
+
+    return customers;
+  }
+}
